9400fc068fbe6ee455431698a1ea8667370a02d3
delon-nazara <delon.gf.nazara@gmail.com>
2024-10-29 17:10:31 +0700

    Add detail screen

--- app/src/main/java/com/example/proyekutslabpemrogramanmobile/adapter/DataAdapter.kt
+++ app/src/main/java/com/example/proyekutslabpemrogramanmobile/adapter/DataAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.proyekutslabpemrogramanmobile.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyekutslabpemrogramanmobile.R
+import com.example.proyekutslabpemrogramanmobile.activity.DetailActivity
 import com.example.proyekutslabpemrogramanmobile.model.DataModel
 
 class DataModelAdapter(private val dataList: List<DataModel>) : RecyclerView.Adapter<DataModelAdapter.DataModelViewHolder>() {
@@ -28,6 +30,22 @@ class DataModelAdapter(private val dataList: List<DataModel>) : RecyclerView.Ada
         holder.imageViewLogo.contentDescription = currentItem.name
         holder.textViewName.text = currentItem.name
         holder.textViewUseCases.text = currentItem.useCases
+
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, DetailActivity::class.java)
+
+            intent.putExtra("LOGO_ID", currentItem.logoId)
+            intent.putExtra("NAME", currentItem.name)
+            intent.putExtra("CREATOR", currentItem.creator)
+            intent.putExtra("YEAR_RELEASED", currentItem.yearReleased)
+            intent.putExtra("FILE_EXTENSION", currentItem.fileExtension)
+            intent.putExtra("USE_CASES", currentItem.useCases)
+            intent.putExtra("DESCRIPTION", currentItem.description)
+            intent.putExtra("DOCUMENTATION_LINK", currentItem.documentationLink)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = dataList.size
